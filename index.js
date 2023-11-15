88870a4a24e1c1a36b44e2b886d2170bf67b0be3
Jonathan Kulak <lackadaize@msn.com>
2023-11-15 04:57:10 -0500

    This is the git commit for the Creating and Insterting DOM Nodes Lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Jonathan Kulak is the champion"
